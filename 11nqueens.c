45f3aa4e2ddccfef6d542c5d73f74ca1d7239f24
saipraneetha <saipraneetha.cs24@bmsce.ac.in>
2026-06-03 00:02:38 +0530

    Add files via upload

---
+++ 11nqueens.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int x[20];
+
+int place(int k, int i)
+{
+    for (int j = 1; j < k; j++)
+    {
+        if (x[j] == i || abs(x[j] - i) == abs(j - k))
+            return 0;
+    }
+    return 1;
+}
+
+void nQueens(int k, int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (place(k, i))
+        {
+            x[k] = i;
+
+            if (k == n)
+            {
+                for (int j = 1; j <= n; j++)
+                    printf("%d ", x[j]);
+                printf("\n");
+            }
+            else
+            {
+                nQueens(k + 1, n);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of queens: ");
+    scanf("%d", &n);
+
+    nQueens(1, n);
+
+    return 0;
+}
